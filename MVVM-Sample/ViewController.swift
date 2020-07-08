80b9a09bb455ce48105444cbbe69a16812f5b250
Alireza <mr.toghyiani@icloud.com>
2020-07-08 11:42:59 +0430

    Initial Commit

---
+++ MVVM-Sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM-Sample
+//
+//  Created by ali on 7/8/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
